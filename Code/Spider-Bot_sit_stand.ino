3ab5f603f617ce3c09f4db371c96046119d4e9e9
Maxime <33199920+DocMarmel@users.noreply.github.com>
2018-03-09 14:04:49 +0100

    Sit and stand
    For the moment stand doesn't work

--- Code/Spider-Bot_sit_stand.ino
+++ Code/Spider-Bot_sit_stand.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 #include <Wire.h>
 
-//déclaration des servo
+//servo's declaration
 Servo servo1;
 Servo servo2;
 Servo servo3;
@@ -22,19 +22,19 @@ Servo servo12;
 
   Serial.begin(9600);
   
-//attachement des servo sur les pin
- servo1.attach(2); //avant droite 1ère partie
- servo2.attach(3); //avant droite 2ème partie
- servo3.attach(4); //avant droite 3ème partie
- servo4.attach(5); //arrière droite 1ère partie
- servo5.attach(6); //arrière droite 2ème partie
- servo6.attach(7); //arrière droite 3ème partie
- servo7.attach(8); //avant gauche 1ère partie
- servo8.attach(9); //avant gauche 2ème partie
- servo9.attach(10); //avant gauche 3ème partie
- servo10.attach(11); //arrière gauche 1ère partie
- servo11.attach(12); //arrière gauche 2ème partie
- servo12.attach(13); //arrière gauche 3ème partie
+//servo's attachment on the pin 
+ servo1.attach(2); //forward right 1st part
+ servo2.attach(3); //forward right 2nd part
+ servo3.attach(4); //forward right 3rd part
+ servo4.attach(5); //back right 1st part
+ servo5.attach(6); //back right 2nd part
+ servo6.attach(7); //back right 3rd part
+ servo7.attach(8); //forward left 1st part
+ servo8.attach(9); //forward left 2nd part
+ servo9.attach(10); //forward left 3rd part
+ servo10.attach(11); //back left 1st part
+ servo11.attach(12); //back left 2nd part
+ servo12.attach(13); //back left 3rd part
 
   servo1.write(90);
   servo2.write(90);
@@ -51,76 +51,76 @@ Servo servo12;
  delay(2000);
 
       //Sit
-    //AvD
+    //fr (forward right)
  
-  for(int AvD2=90; AvD2<150; AvD2++){
-    servo2.write(AvD2);
+  for(int fr2=90; fr2<150; fr2++){ //forward right 2nd part
+    servo2.write(fr2);
     delay(20);
   }
-  for(int AvD1=90; AvD1>0; AvD1--){
-    servo1.write(AvD1);
+  for(int fr1=90; fr1>0; fr1--){ 
+    servo1.write(fr1);
     delay(10);
   }
-  for(int AvD2=150; AvD2>85; AvD2--){
-    servo2.write(AvD2);
+  for(int fr2=150; fr2>85; fr2--){
+    servo2.write(fr2);
     delay(20);
   }
   
-    //Fin AvD
+    //End fr
     //------------------------------------------
-    //ArD
+    //br (back right)
     
-  for(int ArD2=105; ArD2<165; ArD2++){
-    servo5.write(ArD2);
+  for(int br2=105; br2<165; br2++){ //back right 2nd part
+    servo5.write(br2);
     delay(20);
   }
-  for(int ArD1=100; ArD1>10; ArD1--){
-    servo4.write(ArD1);
+  for(int br1=100; br1>10; br1--){
+    servo4.write(br1);
     delay(10);
   }
-  for(int ArD2=165; ArD2>100; ArD2--){
-    servo5.write(ArD2);
+  for(int br2=165; br2>100; br2--){
+    servo5.write(br2);
     delay(20);
   }
 
-    //Fin ArD
+    //End br
     //-------------------------------------------
-    //AvG
+    //fl (forward left)
     
-   for(int AvG2=95; AvG2>40; AvG2--){
-    servo8.write(AvG2);
+   for(int fl2=95; fl2>40; fl2--){ //forward left 2nd part
+    servo8.write(fl2);
     delay(20);
    }
-   for(int AvG1=90; AvG1<179; AvG1++){
-    servo7.write(AvG1);
+   for(int fl1=90; fl1<179; fl1++){
+    servo7.write(fl1);
     delay(10);
    }
-   for(int AvG2=40; AvG2<95; AvG2++){
-    servo8.write(AvG2);
+   for(int fl2=40; fl2<95; fl2++){
+    servo8.write(fl2);
     delay(20);
    }
 
-    //Fin AvG
+    //End fl
     //-------------------------------------------
-    //ArG
+    //bl (back left)
 
-  for(int ArG2=105; ArG2>45; ArG2--){
-    servo11.write(ArG2);
+  for(int bl2=105; bl2>45; bl2--){ //back left 2nd part
+    servo11.write(bl2);
     delay(20);
   }
-  for(int ArG1=90; ArG1<179; ArG1++){
-    servo10.write(ArG1);
+  for(int bl1=90; bl1<179; bl1++){
+    servo10.write(bl1);
     delay(10);
   }
-  for(int ArG2=45; ArG2<105; ArG2++){
-    servo11.write(ArG2);
+  for(int bl2=45; bl2<105; bl2++){
+    servo11.write(bl2);
     delay(20);
   }
-    //Fin ArG
-      //Fin sit
+    //End bl
+      //End sit
       //-------------------------------------------------------------------
       //Stand
-    //AvD
+    //fr
   
  }
 
